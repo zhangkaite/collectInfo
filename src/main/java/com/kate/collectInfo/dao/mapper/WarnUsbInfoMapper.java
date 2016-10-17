4e7060702043430ed3ce3054b86119b7c271d5e7
kate <zhangkaite@126.com>
2016-10-17 22:55:22 +0800

    新增

--- src/main/java/com/kate/collectInfo/dao/mapper/WarnUsbInfoMapper.java
+++ src/main/java/com/kate/collectInfo/dao/mapper/WarnUsbInfoMapper.java
@@ -1,19 +1,13 @@
 package com.kate.collectInfo.dao.mapper;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.kate.collectInfo.service.entity.WarnUsbInfo;
 @Component
 public class WarnUsbInfoMapper extends AbstractMapper<WarnUsbInfo>{
-	
-	
-	
 	public Integer addInfoList(List<WarnUsbInfo> list) throws Exception {
 		setMapperPack("com.kate.collectInfo.dao.mapper.WarnUsbInfoMapper");
 		try {
