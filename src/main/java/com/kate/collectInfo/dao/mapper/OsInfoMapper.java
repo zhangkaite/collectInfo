aabea85a1f0b1ae979808ccbb8b364f5e1099735
kate <zhangkaite@126.com>
2016-10-20 11:12:26 +0800

    新增

--- src/main/java/com/kate/collectInfo/dao/mapper/OsInfoMapper.java
+++ src/main/java/com/kate/collectInfo/dao/mapper/OsInfoMapper.java
@@ -22,28 +22,57 @@ public class OsInfoMapper extends AbstractMapper<OsInfo> {
 		try {
 			for (OsInfo osinfo : list) {
 				OsInfo isExit = queryData(osinfo);
-				if (null != isExit) {
-					osinfo.setId(isExit.getId());
-					osinfo.setUpdateTime(new Date());
-					updataData(osinfo);
-				} else {
-					osinfo.setUpdateTime(new Date());
-					addData(osinfo);
+				//根据ip、mac、操作系统版本号查询数据，如果不存在则说明新增或者变更
+				if (null == isExit) {
 					DiskDriverInfo entity = new DiskDriverInfo();
 					entity.setIp(osinfo.getIp());
 					entity.setMac(osinfo.getMac());
 					DiskDriverInfo diskDriverInfo = diskDriverMapper.queryData(entity);
-					// 添加操作系统预警
-					WarnComputerInfo warnComputerInfo = new WarnComputerInfo();
-					warnComputerInfo.setIp(osinfo.getIp());
-					warnComputerInfo.setMac(osinfo.getMac());
-					warnComputerInfo.setSerialNumber(diskDriverInfo.getSerialNumber());
-					warnComputerInfo.setOs(osinfo.getVersion());
-					warnComputerInfo.setStatus("变更");
-					warnComputerInfo.setUpdateTime(new Date());
-					warnComputerInfo.setChangeField("操作系统版本号");
-					warnCompInfoMapper.setMapperPack("com.kate.collectInfo.dao.mapper.WarnCompInfoMapper");
-					warnCompInfoMapper.addData(warnComputerInfo);
+					//根据ip、mac查询现有的系统数据是否存在
+					OsInfo ipMacExit=queryData(osinfo, "selectIpMac");
+					if (null!=ipMacExit) {
+						// 添加操作系统预警
+						WarnComputerInfo warnComputerInfo = new WarnComputerInfo();
+						warnComputerInfo.setIp(osinfo.getIp());
+						warnComputerInfo.setMac(osinfo.getMac());
+						warnComputerInfo.setSerialNumber(diskDriverInfo.getSerialNumber());
+						warnComputerInfo.setOs(osinfo.getVersion());
+						warnComputerInfo.setStatus("变更");
+						warnComputerInfo.setUpdateTime(new Date());
+						warnComputerInfo.setChangeField("操作系统版本号");
+						warnCompInfoMapper.setMapperPack("com.kate.collectInfo.dao.mapper.WarnCompInfoMapper");
+						warnCompInfoMapper.addData(warnComputerInfo);
+					}
+					
+					OsInfo ipVersionExit=queryData(osinfo, "selectIpVersion");
+					if (null!=ipVersionExit) {
+						WarnComputerInfo warnComputerInfo = new WarnComputerInfo();
+						warnComputerInfo.setIp(osinfo.getIp());
+						warnComputerInfo.setMac(osinfo.getMac());
+						warnComputerInfo.setSerialNumber(diskDriverInfo.getSerialNumber());
+						warnComputerInfo.setOs(osinfo.getVersion());
+						warnComputerInfo.setStatus("变更");
+						warnComputerInfo.setUpdateTime(new Date());
+						warnComputerInfo.setChangeField("MAC");
+						warnCompInfoMapper.setMapperPack("com.kate.collectInfo.dao.mapper.WarnCompInfoMapper");
+						warnCompInfoMapper.addData(warnComputerInfo);
+					}
+					OsInfo macVersionExit=queryData(osinfo, "selectMacVersion");
+					if (null!=macVersionExit) {
+						WarnComputerInfo warnComputerInfo = new WarnComputerInfo();
+						warnComputerInfo.setIp(osinfo.getIp());
+						warnComputerInfo.setMac(osinfo.getMac());
+						warnComputerInfo.setSerialNumber(diskDriverInfo.getSerialNumber());
+						warnComputerInfo.setOs(osinfo.getVersion());
+						warnComputerInfo.setStatus("变更");
+						warnComputerInfo.setUpdateTime(new Date());
+						warnComputerInfo.setChangeField("IP");
+						warnCompInfoMapper.setMapperPack("com.kate.collectInfo.dao.mapper.WarnCompInfoMapper");
+						warnCompInfoMapper.addData(warnComputerInfo);
+					}
+					osinfo.setUpdateTime(new Date());
+					addData(osinfo);
+					
 				}
 
 			}
